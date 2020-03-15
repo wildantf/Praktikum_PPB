be01b6055b960bbb030419973f14673665328bfb
wildantf <wildantammafazchair877@gmail.com>
2020-03-15 16:12:13 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/praktikum_ppb/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.dicoding.picodiploma.praktikum_ppb.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
